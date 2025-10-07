76937afd9062fde56b83ca4b3da2cf73446e787f
PeDitX <pedram.ale@me.com>
2025-10-07 19:42:59 +0330

    Update runner via web updater

--- services/service_runner.sh
+++ services/service_runner.sh
@@ -6,97 +6,97 @@
 ACTION="$1"
 
 # --- Dynamic URLs Start ---
-URL_INSTALLDNSJUMPER="https://peditx.ir/projects/DNSJumper/code/install.sh"
-URL_PXNOTIFIER="https://peditx.ir/projects/PXnotifier/code/install.sh"
-URL_TORPLUS="https://raw.githubusercontent.com/peditx/openwrt-torplus/main/.Files/install.sh"
-URL_SSHPLUS="https://raw.githubusercontent.com/peditx/SshPlus/main/Files/install_sshplus.sh"
-URL_AIRCAST="https://raw.githubusercontent.com/peditx/aircast-openwrt/main/aircast_install.sh"
-URL_WARP="https://raw.githubusercontent.com/peditx/openwrt-warpplus/refs/heads/main/files/install.sh"
-URL_WARPPLUSPLUS="https://raw.githubusercontent.com/peditx/openwrt-warpplusplus/refs/heads/main/install.sh"
-URL_AMNEZIAWG="https://raw.githubusercontent.com/Slava-Shchipunov/awg-openwrt/refs/heads/master/amneziawg-install.sh"
-URL_IRANIPS="https://raw.githubusercontent.com/peditx/iranIPS/refs/heads/main/.files/iranips.sh"
-URL_CARBONPX="https://raw.githubusercontent.com/peditx/luci-theme-carbonpx/refs/heads/main/install.sh"
-URL_PEDITX="https://raw.githubusercontent.com/peditx/luci-theme-peditx/refs/heads/main/install.sh"
-URL_INSTALLAURORATHEME="https://peditx.ir/foreignscs/luci-theme-aurora/install.sh"
-URL_INSTALLARGONTHEME="https://peditx.ir/foreignscs/luci-theme-argon/install.sh"
+URL_INSTALL_INSTALLDNSJUMPER="https://peditx.ir/projects/DNSJumper/code/install.sh"
+URL_INSTALL_PXNOTIFIER="https://peditx.ir/projects/PXnotifier/code/install.sh"
+URL_INSTALL_TORPLUS="https://raw.githubusercontent.com/peditx/openwrt-torplus/main/.Files/install.sh"
+URL_INSTALL_SSHPLUS="https://raw.githubusercontent.com/peditx/SshPlus/main/Files/install_sshplus.sh"
+URL_INSTALL_AIRCAST="https://raw.githubusercontent.com/peditx/aircast-openwrt/main/aircast_install.sh"
+URL_INSTALL_WARP="https://raw.githubusercontent.com/peditx/openwrt-warpplus/refs/heads/main/files/install.sh"
+URL_INSTALL_WARPPLUSPLUS="https://raw.githubusercontent.com/peditx/openwrt-warpplusplus/refs/heads/main/install.sh"
+URL_INSTALL_AMNEZIAWG="https://raw.githubusercontent.com/Slava-Shchipunov/awg-openwrt/refs/heads/master/amneziawg-install.sh"
+URL_INSTALL_IRANIPS="https://raw.githubusercontent.com/peditx/iranIPS/refs/heads/main/.files/iranips.sh"
+URL_INSTALL_CARBONPX="https://raw.githubusercontent.com/peditx/luci-theme-carbonpx/refs/heads/main/install.sh"
+URL_INSTALL_PEDITX="https://raw.githubusercontent.com/peditx/luci-theme-peditx/refs/heads/main/install.sh"
+URL_INSTALL_INSTALLAURORATHEME="https://peditx.ir/foreignscs/luci-theme-aurora/install.sh"
+URL_INSTALL_INSTALLARGONTHEME="https://peditx.ir/foreignscs/luci-theme-argon/install.sh"
 # --- Dynamic URLs End ---
 
 # --- Dynamic Uninstall URLs Start ---
-URL_UNINSTALL_INSTALLDNSJUMPER="https://peditx.ir/projects/DNSJumper/code/uninstall.sh"
+URL_UNINSTALL_INSTALL_INSTALLDNSJUMPER="https://www.google.com/search?q=https://peditx.ir/projects/DNSJumper/code/uninstall.sh"
 # --- Dynamic Uninstall URLs End ---
 
 
 # --- Dynamic Functions Start ---
 install_installdnsjumper() {
 	echo "Downloading Install DNSJumper components..."
-	cd /tmp && rm -f install.sh && wget -q "$URL_INSTALLDNSJUMPER" -O install.sh && chmod +x install.sh && sh install.sh
+	cd /tmp && rm -f install.sh && wget -q "$URL_INSTALL_INSTALLDNSJUMPER" -O install.sh && chmod +x install.sh && sh install.sh
 }
 
 install_pxnotifier() {
 	echo "Downloading PXNotifier components..."
-	cd /tmp && rm -f install.sh && wget -q "$URL_PXNOTIFIER" -O install.sh && chmod +x install.sh && sh install.sh
+	cd /tmp && rm -f install.sh && wget -q "$URL_INSTALL_PXNOTIFIER" -O install.sh && chmod +x install.sh && sh install.sh
 }
 
 install_torplus() {
 	echo "Downloading Install TORPlus components..."
-	cd /tmp && rm -f install.sh && wget -q "$URL_TORPLUS" -O install.sh && chmod +x install.sh && sh install.sh
+	cd /tmp && rm -f install.sh && wget -q "$URL_INSTALL_TORPLUS" -O install.sh && chmod +x install.sh && sh install.sh
 }
 
 install_sshplus() {
 	echo "Downloading Install SSHPlus components..."
-	cd /tmp && rm -f install_sshplus.sh && wget -q "$URL_SSHPLUS" -O install_sshplus.sh && chmod +x install_sshplus.sh && sh install_sshplus.sh
+	cd /tmp && rm -f install_sshplus.sh && wget -q "$URL_INSTALL_SSHPLUS" -O install_sshplus.sh && chmod +x install_sshplus.sh && sh install_sshplus.sh
 }
 
 install_aircast() {
 	echo "Downloading Install Air-Cast components..."
-	cd /tmp && rm -f aircast_install.sh && wget -q "$URL_AIRCAST" -O aircast_install.sh && chmod +x aircast_install.sh && sh aircast_install.sh
+	cd /tmp && rm -f aircast_install.sh && wget -q "$URL_INSTALL_AIRCAST" -O aircast_install.sh && chmod +x aircast_install.sh && sh aircast_install.sh
 }
 
 install_warp() {
 	echo "Downloading Install Warp+ components..."
-	cd /tmp && rm -f install.sh && wget -q "$URL_WARP" -O install.sh && chmod +x install.sh && sh install.sh
+	cd /tmp && rm -f install.sh && wget -q "$URL_INSTALL_WARP" -O install.sh && chmod +x install.sh && sh install.sh
 }
 
 install_warpplusplus() {
 	echo "Downloading Install Warp++ components..."
-	cd /tmp && rm -f install.sh && wget -q "$URL_WARPPLUSPLUS" -O install.sh && chmod +x install.sh && sh install.sh
+	cd /tmp && rm -f install.sh && wget -q "$URL_INSTALL_WARPPLUSPLUS" -O install.sh && chmod +x install.sh && sh install.sh
 }
 
 install_amneziawg() {
 	echo "Downloading Install AmneziaWG components..."
-	cd /tmp && rm -f amneziawg-install.sh && wget -q "$URL_AMNEZIAWG" -O amneziawg-install.sh && chmod +x amneziawg-install.sh && sh amneziawg-install.sh
+	cd /tmp && rm -f amneziawg-install.sh && wget -q "$URL_INSTALL_AMNEZIAWG" -O amneziawg-install.sh && chmod +x amneziawg-install.sh && sh amneziawg-install.sh
 }
 
 install_iranips() {
 	echo "Downloading Install Iran Rule IPS for Passwall2 components..."
-	cd /tmp && rm -f iranips.sh && wget -q "$URL_IRANIPS" -O iranips.sh && chmod +x iranips.sh && sh iranips.sh
+	cd /tmp && rm -f iranips.sh && wget -q "$URL_INSTALL_IRANIPS" -O iranips.sh && chmod +x iranips.sh && sh iranips.sh
 }
 
 install_carbonpx() {
 	echo "Downloading Install CarbonPX Theme components..."
-	cd /tmp && rm -f install.sh && wget -q "$URL_CARBONPX" -O install.sh && chmod +x install.sh && sh install.sh
+	cd /tmp && rm -f install.sh && wget -q "$URL_INSTALL_CARBONPX" -O install.sh && chmod +x install.sh && sh install.sh
 }
 
 install_peditx() {
 	echo "Downloading Install PeDitX Theme components..."
-	cd /tmp && rm -f install.sh && wget -q "$URL_PEDITX" -O install.sh && chmod +x install.sh && sh install.sh
+	cd /tmp && rm -f install.sh && wget -q "$URL_INSTALL_PEDITX" -O install.sh && chmod +x install.sh && sh install.sh
 }
 
 install_installauroratheme() {
 	echo "Downloading Install Aurora Theme components..."
-	cd /tmp && rm -f install.sh && wget -q "$URL_INSTALLAURORATHEME" -O install.sh && chmod +x install.sh && sh install.sh
+	cd /tmp && rm -f install.sh && wget -q "$URL_INSTALL_INSTALLAURORATHEME" -O install.sh && chmod +x install.sh && sh install.sh
 }
 
 install_installargontheme() {
 	echo "Downloading Install Argon Theme components..."
-	cd /tmp && rm -f install.sh && wget -q "$URL_INSTALLARGONTHEME" -O install.sh && chmod +x install.sh && sh install.sh
+	cd /tmp && rm -f install.sh && wget -q "$URL_INSTALL_INSTALLARGONTHEME" -O install.sh && chmod +x install.sh && sh install.sh
 }
 # --- Dynamic Functions End ---
 
 # --- Dynamic Uninstall Functions Start ---
 uninstall_installdnsjumper() {
-	echo "Downloading Uninstall DNSJumper components..."
-	cd /tmp && rm -f uninstall.sh && wget -q "$URL_UNINSTALL_INSTALLDNSJUMPER" -O uninstall.sh && chmod +x uninstall.sh && sh uninstall.sh
+	echo "Downloading Uninstall Install DNSJumper components..."
+	cd /tmp && rm -f uninstall.sh && wget -q "$URL_UNINSTALL_INSTALL_INSTALLDNSJUMPER" -O uninstall.sh && chmod +x uninstall.sh && sh uninstall.sh
 }
 # --- Dynamic Uninstall Functions End ---
 
