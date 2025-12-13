177f2084555a01ee47d6d05903557ed48a881273
PeDitX <pedram.ale@me.com>
2025-12-13 19:38:48 +0330

    Update runner via web updater

--- services/service_runner.sh
+++ services/service_runner.sh
@@ -12,6 +12,7 @@ URL_INSTALL_INSTALLAIRCAST="https://raw.githubusercontent.com/peditx/aircast-ope
 URL_INSTALL_ADGUARDHOME="https://peditx.ir/projects/AdGuard-Home/install.sh"
 URL_INSTALL_NEXTCLOUD="https://peditx.ir/projects/NextCloud/install.sh"
 URL_INSTALL_HOMEASSISTANT="https://peditx.ir/projects/Home%20Assistant-x86-pi/install.sh"
+URL_INSTALL_MOSQUITTOMQTT="https://peditx.ir/projects/Mosquitto/install.sh"
 URL_INSTALL_PORTAINER="https://peditx.ir/projects/Portainer-x86-pi/install.sh"
 URL_INSTALL_DOCKER="https://peditx.ir/projects/docker/install.sh"
 URL_INSTALL_WIREGUARDPLUS="https://peditx.ir/projects/WireGuard-Plus/install.sh"
@@ -39,6 +40,7 @@ URL_UNINSTALL_INSTALLAIRCAST="https://raw.githubusercontent.com/peditx/aircast-o
 URL_UNINSTALL_ADGUARDHOME="https://peditx.ir/projects/AdGuard-Home/uninstall.sh"
 URL_UNINSTALL_NEXTCLOUD="https://peditx.ir/projects/NextCloud/uninstall.sh"
 URL_UNINSTALL_HOMEASSISTANT="https://peditx.ir/projects/Home%20Assistant-x86-pi/uninstall.sh"
+URL_UNINSTALL_MOSQUITTOMQTT="https://peditx.ir/projects/Mosquitto/uninstall.sh"
 URL_UNINSTALL_PORTAINER="https://peditx.ir/projects/Portainer-x86-pi/uninstall.sh"
 URL_UNINSTALL_DOCKER="https://peditx.ir/projects/docker/uninstall.sh"
 URL_UNINSTALL_WIREGUARDPLUS="https://peditx.ir/projects/WireGuard-Plus/uninstall.sh"
@@ -85,6 +87,11 @@ install_nextcloud() {
 install_homeassistant() {
 	echo "Downloading Home Assistant components..."
 	cd /tmp && rm -f install.sh && wget -q "$URL_INSTALL_HOMEASSISTANT" -O install.sh && chmod +x install.sh && sh install.sh
+}
+
+install_mosquittomqtt() {
+	echo "Downloading Mosquitto mqtt components..."
+	cd /tmp && rm -f install.sh && wget -q "$URL_INSTALL_MOSQUITTOMQTT" -O install.sh && chmod +x install.sh && sh install.sh
 }
 
 install_portainer() {
@@ -207,6 +214,11 @@ uninstall_nextcloud() {
 uninstall_homeassistant() {
 	echo "Downloading Uninstall Home Assistant components..."
 	cd /tmp && rm -f uninstall.sh && wget -q "$URL_UNINSTALL_HOMEASSISTANT" -O uninstall.sh && chmod +x uninstall.sh && sh uninstall.sh
+}
+
+uninstall_mosquittomqtt() {
+	echo "Downloading Uninstall Mosquitto mqtt components..."
+	cd /tmp && rm -f uninstall.sh && wget -q "$URL_UNINSTALL_MOSQUITTOMQTT" -O uninstall.sh && chmod +x uninstall.sh && sh uninstall.sh
 }
 
 uninstall_portainer() {
@@ -345,6 +357,8 @@ case "$ACTION" in
 	uninstall_nextcloud) uninstall_nextcloud ;;
 	install_homeassistant) install_homeassistant ;;
 	uninstall_homeassistant) uninstall_homeassistant ;;
+	install_mosquittomqtt) install_mosquittomqtt ;;
+	uninstall_mosquittomqtt) uninstall_mosquittomqtt ;;
 	install_portainer) install_portainer ;;
 	uninstall_portainer) uninstall_portainer ;;
 	install_docker) install_docker ;;
